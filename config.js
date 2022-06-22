d792bfeef6df35383755db90a4885f816a02d1d6
LETRANG12 <108001252+LETRANG12@users.noreply.github.com>
2022-06-22 19:20:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    titleWeb: "hello!! em là Rùa nè",
+    introTitle: 'Hello anh ! em là Rùa nè :3',
+    introDesc: `Hỏi này nhaaaa`,
+    btnIntro: 'Hỏi đi...',
+    title: 'Anh thích em đúng khoongggg ?',
+    desc: 'Trả lời thật điiii hehe ',
+    btnYes: 'Thích lắm lắm :3',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    question: 'Cho em biết lí do anh thích em nào???',
+    btnReply: 'Gửi cho Rùa ',
+    reply: 'Em đáng yêu vl',
+    mess: 'Em biết mà hahahaha',
+    messDesc: 'Thế giờ thích thì phải nói đi thôiiii',
+    btnAccept: 'okiii :3',
+   
 }
